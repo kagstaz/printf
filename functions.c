f2876e5244ab6f9ab2baaad8ac9e433e3891ce7e
kagstaz <edwardkags@yahoo.co.uk>
2023-09-19 15:15:05 +0300

    main.h

--- functions.c
+++ functions.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/************************* PRINT CHAR *************************/
+/*** PRINT CHAR ***/
 
 /**
  * print_char - Prints a char
@@ -19,7 +19,7 @@ int print_char(va_list types, char buffer[],
 
 	return (handle_write_char(c, buffer, flags, width, precision, size));
 }
-/************************* PRINT A STRING *************************/
+/*** PRINT A STRING ***/
 
 /**
  * print_string - Prints a string
@@ -75,11 +75,11 @@ int print_string(va_list types, char buffer[],
 
 	return (write(1, str, length));
 }
-/************************* PRINT PERCENT SIGN *************************/
+/*** PRINT PERCENT SIGN ***/
 
 /**
  * print_percent - Prints a percent sign
- * @types: Lista of arguments
+ * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
@@ -99,11 +99,11 @@ int print_percent(va_list types, char buffer[],
 	return (write(1, "%%", 1));
 }
 
-/************************* PRINT INT *************************/
+/*** PRINT INT ***/
 
 /**
  * print_int - Print int
- * @types: Lista of arguments
+ * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
@@ -144,11 +144,11 @@ int print_int(va_list types, char buffer[],
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
-/************************* PRINT BINARY *************************/
+/*** PRINT BINARY ***/
 
 /**
  * print_binary - Prints an unsigned number
- * @types: Lista of arguments
+ * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width.
