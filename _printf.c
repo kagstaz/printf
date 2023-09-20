5220bb0b9b13847bf603895efeedddf081fcfa67
Mosestack <125935450+Mosestack@users.noreply.github.com>
2023-09-20 03:54:50 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format specifier.
- * Return: Printed chars.
+ * Return: Printed chars only.
  */
 int _printf(const char *format, ...)
 {
